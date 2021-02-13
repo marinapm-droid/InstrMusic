ba2118b8a67e9cfd48812bef131c18d08c500ef6
chinguino <78853705+chinguino@users.noreply.github.com>
2021-02-13 13:57:10 +0000

    cócó

--- app/src/main/java/com/example/instrmusic3/Login.java
+++ app/src/main/java/com/example/instrmusic3/Login.java
@@ -2,8 +2,11 @@ package com.example.instrmusic3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        Button button = (Button) findViewById(R.id.goToSignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), SignUp.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
+
+
 }
