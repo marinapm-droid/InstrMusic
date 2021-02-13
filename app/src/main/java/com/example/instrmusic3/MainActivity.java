832ee36542b55cf8e13c17ca49a22255feab37a0
marinapm-droid <marina_pereira_martins@hotmail.com>
2021-02-13 00:00:02 +0000

    Initial commit

---
+++ app/src/main/java/com/example/instrmusic3/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.instrmusic3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN= 5000; //5 sec
+    Animation topAnim, bottomAnim;
+    ImageView nome, barras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Hooks
+        nome=findViewById(R.id.nome);
+        barras=findViewById(R.id.barras);
+
+        barras.setAnimation(topAnim);
+        nome.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, Login.class);
+
+                Pair[] pairs= new Pair[2];
+                pairs[0]=new Pair<View, String>(nome, "logo_nome");
+                pairs[1]=new Pair<View, String>(barras, "logo_barras");
+
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                    startActivity(intent, options.toBundle());
+                    finish();
+                } else {
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                }
+            }
+        }, SPLASH_SCREEN);
+
+    }
+}
