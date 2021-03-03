bf0901c3933526361a3cd53490ae32f3b2f8ce43
marinapm-droid <marina_pereira_martins@hotmail.com>
2021-03-03 18:07:28 +0000

    babaui

--- app/src/main/java/com/example/instrmusic3/HomePage.java
+++ app/src/main/java/com/example/instrmusic3/HomePage.java
@@ -15,7 +15,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.instrmusic3.activities.SettingsActivity;
 import com.example.instrmusic3.activities.StartUpActivity;
+import com.example.instrmusic3.activities.StartUpEffectActivity;
 import com.example.instrmusic3.activities.StartUpSoundActivity;
 import com.example.instrmusic3.auth.Login;
 import com.example.instrmusic3.auth.SignUp;
@@ -27,30 +29,53 @@ import com.google.firebase.database.ValueEventListener;
 
 public class HomePage extends Activity {
 
-    TextView sensorBtn, effectBtn, soundBtn, settingBtn;
+    TextView sensorBtn, effectBtn, soundBtn, settingsBtn, logOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // fullscreen
         setContentView(R.layout.activity_home);
-        //sensorBtn = findViewById(R.id.sensors);
-        //effectBtn = findViewById(R.id.effects);
-        //soundBtn = findViewById(R.id.sounds);
-        //soundBtn = findViewById(R.id.settings);
+        sensorBtn = findViewById(R.id.sensorImg);
+        effectBtn = findViewById(R.id.effectsImg);
+        soundBtn = findViewById(R.id.soundsImg);
+        settingsBtn = findViewById(R.id.settingsImg);
+        logOutBtn = findViewById(R.id.logOutImg);
 
 
-
-
-       /* sensorBtn.setOnClickListener(new View.OnClickListener() {
-
+        sensorBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(HomePage.this, StartUpActivity.class);
                 startActivity(intent);
             }
+        });
 
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        });*/
+        soundBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, StartUpSoundActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        effectBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, StartUpEffectActivity.class);
+                startActivity(intent);
+            }
+        });
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
