d3919ee53773d06e07e132f624595f12d034e467
marinapm-droid <marina_pereira_martins@hotmail.com>
2021-03-10 18:47:29 +0000

    babaui

--- app/src/main/java/com/example/instrmusic3/auth/UserHelperClass.java
+++ app/src/main/java/com/example/instrmusic3/auth/UserHelperClass.java
@@ -2,7 +2,7 @@ package com.example.instrmusic3.auth;
 
 public class UserHelperClass {
 
-    String nome, email, password, confPassword, effect, sound, sensor;
+    String nome,email, password, confPassword, sound, effect, sensor;
 
 
     public UserHelperClass(String nome, String email, String password, String confPassword) {
@@ -12,10 +12,53 @@ public class UserHelperClass {
         this.confPassword = confPassword;
     }
 
-    public UserHelperClass(String effect, String sound, String sensor) {
+    public UserHelperClass(String effect, String sensor, String sound) {
         this.effect = effect;
-        this.sound = sound;
         this.sensor = sensor;
+        this.sound = sound;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEffect() {
+        return effect;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public String getSensor() {
+        return sensor;
+    }
+
+    // public void setNome(String nome) {
+     //   this.nome = nome;
+   // }
+
+    public String getEmail() {
+        return email;
+    }
+
+    //    public void setEmail(String email) {
+    //        this.email = email;
+    //    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //  public void setPassword(String password) {
+    //      this.password = password;
+    //  }
+
+    public String getConfPassword() {
+        return confPassword;
+    }
+
+    //   public void setConfPassword(String confPassword) {
+    //      this.confPassword = confPassword;
+    //    }
 }
