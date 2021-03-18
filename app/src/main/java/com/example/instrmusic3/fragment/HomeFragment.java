8d19a17bffd021fa700c7e7d12f7af414b6aaac7
marinapm-droid <marina_pereira_martins@hotmail.com>
2021-03-18 15:42:31 +0000

    babaui

--- app/src/main/java/com/example/instrmusic3/fragment/HomeFragment.java
+++ app/src/main/java/com/example/instrmusic3/fragment/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.instrmusic3.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +13,96 @@ import androidx.fragment.app.Fragment;
 
 import com.example.instrmusic3.HomePage;
 import com.example.instrmusic3.R;
+import com.example.instrmusic3.dispatch.OscCommunication;
+import com.example.instrmusic3.dispatch.OscConfiguration;
+import com.example.instrmusic3.dispatch.OscHandler;
+import com.example.instrmusic3.sensors.Settings;
+import com.illposed.osc.OSCMessage;
+import com.illposed.osc.OSCPortOut;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private Handler handler = new Handler();
+    private Settings settings;
+    static int onOff = 0;
+    public Settings getSettings() {
+        return this.settings;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        HomePage activity = (HomePage) getActivity();
+        activity.getSettings();
+        this.settings = activity.loadSettings();
+
         View v = inflater.inflate(R.layout.activity_home, container, false);
         CompoundButton activeButton = v.findViewById(R.id.active);
-        HomePage activity = (HomePage) getActivity();
         activeButton.setOnCheckedChangeListener(activity);
+        activeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onOff == 0) {
+                    onOff = 1;
+                    OscCommunication communication = new OscCommunication("OSC dispatcher thread", Thread.MIN_PRIORITY);
+                    communication.start();
+                    OscHandler handler = communication.getOscHandler();
+                    OscConfiguration oscConfiguration = OscConfiguration.getInstance();
+                    OSCPortOut sender = oscConfiguration.getOscPort();
+                    OSCMessage msg = new OSCMessage("/play");
+                    try {
+                        sender.send(msg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    OscCommunication communication = new OscCommunication("OSC dispatcher thread", Thread.MIN_PRIORITY);
+                    communication.start();
+                    OscHandler handler = communication.getOscHandler();
+                    OscConfiguration oscConfiguration = OscConfiguration.getInstance();
+                    OSCPortOut sender = oscConfiguration.getOscPort();
+                    OSCMessage msg = new OSCMessage("/pause");
+                    try {
+                        sender.send(msg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    onOff = 0;
+                }
+            }
+        });
+
+
+        activeButton.setOnCheckedChangeListener(activity);
+        CompoundButton stopButton = v.findViewById(R.id.stop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onOff == 0) {
+                    onOff = 1;
+                    OscCommunication communication = new OscCommunication("OSC dispatcher thread", Thread.MIN_PRIORITY);
+                    communication.start();
+                    OscHandler handler = communication.getOscHandler();
+                    OscConfiguration oscConfiguration = OscConfiguration.getInstance();
+                    OSCPortOut sender = oscConfiguration.getOscPort();
+                    OSCMessage msg = new OSCMessage("/stop");
+                    try {
+                        sender.send(msg);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    onOff = 0;
+                }
+            }
+        });
         return v;
     }
 
